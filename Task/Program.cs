a8250b3f5eec34a334e4b31ea064ead9e121f5ea
Sergey <neowaps1@mail.ru>
2023-06-05 01:34:01 +0300

    create method FillArray

---
+++ Task/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите количество строк: ");
+int countArr = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[countArr];
+void FillArray()
+{
+    for (int i = 0; i < countArr; i++)
+    {
+        System.Console.Write($"Введите {i + 1}'ю строку: ");
+        array[i] = Console.ReadLine();
+    }
+}
