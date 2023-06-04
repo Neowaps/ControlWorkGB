937079e0cb25307fc9f89febadace1e84acc5a53
Sergey <neowaps1@mail.ru>
2023-06-05 01:46:53 +0300

    create method FormArray

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,5 +1,6 @@
 ﻿System.Console.Write("Введите количество строк: ");
 int countArr = Convert.ToInt32(Console.ReadLine());
+int limit = 3;
 string[] array = new string[countArr];
 void FillArray()
 {
@@ -7,5 +8,18 @@ void FillArray()
     {
         System.Console.Write($"Введите {i + 1}'ю строку: ");
         array[i] = Console.ReadLine();
+    }
+}
+
+void FormArray(string[] arrayForm)
+{
+    for (int i = 0; i < countArr; i++)
+    {
+        if (array.Length <= limit)
+        {
+            int count = 0;
+            arrayForm[count] = array[i];
+            count++;
+        }
     }
 }
